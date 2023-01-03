0c03d3aa6447c23349bb8458b51598f478126a86
TheStupidDev <98951500+TheStupidDev@users.noreply.github.com>
2023-01-03 13:57:21 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-var i = "gatau ntar gw tambahin yang keren-keren dah";
+var gw = "gatau mau nambahin apa ntar gw tambahin yang keren-keren dah";
+
+
+function checkMobile(){
+if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+	console.log("mobile")
+  alert("Lu lagi make HP yaakann? Perlu gw note untuk sekarang design webnya di HP masih agak jelek yaa, lagi gw usahain untuk bikin jadi responsive biar di HP juga rapi. Makacii..");
+}else{
+	console.log("pc")
+}
+}
