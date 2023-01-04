10f89d76eb04bfdbca6102c4175f03cda8549367
TheStupidDev <98951500+TheStupidDev@users.noreply.github.com>
2023-01-04 18:39:17 +0700

    mobile button fix

--- script.js
+++ script.js
@@ -13,7 +13,8 @@ if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigat
   alert("Lu lagi make HP yaakann?\n\nPerlu gw note untuk sekarang design webnya di HP masih agak jelek yaa (beberapa gambar & element diapus dan/atau masih kurang rapi), lagi gw usahain untuk bikin jadi responsive biar di HP juga rapi. \n\n(P.S: Kalo mau liat versi originalnya bisa pencet 'desktop site' lewat browser lu). \n\nMakacii..");
   hideElem("tentang_gw")
   document.getElementById('introButton').onclick = function () {
-		location.href = "#tentang_gw_mobile";
+	  //location.href = "#tentang_gw_mobile";  -- P.S: Kok ga bisa sih ;(
+	  location.href = "https://thestupiddev.github.io/#tentang_gw_mobile";
 	}
 }
 else{
