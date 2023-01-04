2846c149f6b80ea23d9570cffcc97ba5f6a74f68
TheStupidDev <98951500+TheStupidDev@users.noreply.github.com>
2023-01-04 14:34:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-var gw = "lagi gatau mau nambahin apa ntar gw tambahin yang keren-keren dah";
-
+function hideElem(elemID) {
+  var x = document.getElementById(elemID);
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
 
 function checkMobile(){
 if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
 	console.log("mobile")
-  alert("Lu lagi make HP yaakann? Perlu gw note untuk sekarang design webnya di HP masih agak jelek yaa, lagi gw usahain untuk bikin jadi responsive biar di HP juga rapi. Makacii..");
-}else{
-	console.log("pc")
+  alert("Lu lagi make HP yaakann? Perlu gw note untuk sekarang design webnya di HP masih agak jelek yaa (beberapa gambar & element diapus dan/atau masih kurang rapi), lagi gw usahain untuk bikin jadi responsive biar di HP juga rapi. \n\n(P.S: Kalo mau liat versi originalnya bisa pencet 'desktop site' lewat browser lu). Makacii..");
+  hideElem("tentang_gw")
+  document.getElementById('introButton').onclick = function () {
+		location.href = "#tentang_gw_mobile";
+	}
 }
+else{
+	console.log("pc");
+	hideElem("tentang_gw_mobile")
+	}
 }
