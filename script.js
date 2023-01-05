cd875e0a2fa2db40438a88a4a9c93e74e279b647
TheStupidDev <98951500+TheStupidDev@users.noreply.github.com>
2023-01-05 18:34:49 +0700

    button fix (global)

--- script.js
+++ script.js
@@ -9,16 +9,12 @@ function hideElem(elemID) {
 
 function checkMobile(){
 if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-	console.log("mobile")
+  console.log("mobile")
   alert("Lu lagi make HP yaakann?\n\nPerlu gw note untuk sekarang design webnya di HP masih agak jelek yaa (beberapa gambar & element diapus dan/atau masih kurang rapi), lagi gw usahain untuk bikin jadi responsive biar di HP juga rapi. \n\n(P.S: Kalo mau liat versi originalnya bisa pencet 'desktop site' lewat browser lu). \n\nMakacii..");
   hideElem("tentang_gw")
-  document.getElementById('introButton').onclick = function () {
-	  //location.href = "#tentang_gw_mobile";  -- P.S: Kok ga bisa sih ;(
-	  location.href = "https://thestupiddev.github.io/#tentang_gw_mobile";
-	}
 }
 else{
-	console.log("pc");
-	hideElem("tentang_gw_mobile")
-	}
+  console.log("pc");
+  hideElem("tentang_gw_mobile")
+ }
 }
